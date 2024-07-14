addeca9fba0c7e299727aa990844a10cf09e10e4
NathanWMartins <nathanwillmartins@gmail.com>
2024-07-13 21:51:20 -0300

    feat: Melhora do Design, tratamento de campeonatos não necessários e começo da tela visualizar Tips

--- app/components/ActualTips.tsx
+++ app/components/ActualTips.tsx
@@ -1,9 +1,45 @@
+'use client'
+
+import { useEffect, useState } from "react";
 import { Card, CardBody, Divider } from "@nextui-org/react";
 import Image from "next/image";
+import { useAuthContext } from "../context/AuthContext";
+import { getActualTipsFromUser, getUserByAuthId } from "../firebase/user";
+
+interface UserTip {
+  matchId: string;
+  selectedTeam: string;
+}
 
 const ActualTips = () => {
+  const [actualTips, setActualTips] = useState<UserTip[]>([]);
+  const [loading, setLoading] = useState(true);
+  const { userAuth } = useAuthContext();
+
+  useEffect(() => {
+    const fetchUserTips = async () => {
+      if (userAuth) {
+        try {
+          const user = await getUserByAuthId(userAuth.uid);
+          const tips = await getActualTipsFromUser(user.id);
+          setActualTips(tips || []);
+        } catch (error) {
+          console.error("Erro ao buscar palpites do usuário:", error);
+        } finally {
+          setLoading(false);
+        }
+      }
+    };
+
+    fetchUserTips();
+  }, [userAuth]);
+
+  if (loading) {
+    return <div className="text-white">Carregando...</div>;
+  }
+
   return (
-    <div className="flex flex-col h-full w-full bg-neutral-900">
+    <div className="flex flex-col min-h-screen w-full bg-neutral-900">
       <Card className="m-2 bg-zinc-700">
         <CardBody>
           <div className="flex">
@@ -24,6 +60,31 @@ const ActualTips = () => {
           </div>
           <div className="px-1">
             <Divider className="bg-yellow-400" />
+          </div>
+          <div className="mt-4">
+            {actualTips.length > 0 ? (
+              <div className="text-white">
+                {/* Tem que alterar o design mas é contigo, só deixei assim, por que as informações já tao de bandeja */}
+                <h2>Suas Apostas:</h2>
+                <ul>
+                  {actualTips.map((tip, index) => (
+                    <li key={index}>
+                      Match ID: {tip.matchId}, Selected Team: {tip.selectedTeam}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ) : (
+              <div className="flex flex-col items-center justify-center text-white h-64">
+                <Image
+                  src={"/AvisoApostaNaoEncontrada.png"}
+                  alt={"Aviso Aposta Nao Encontrada"}
+                  width={50}
+                  height={50}
+                  className="object-cover"
+                />
+              </div>
+            )}
           </div>
         </CardBody>
       </Card>
--- app/components/LiveMatches.tsx
+++ app/components/LiveMatches.tsx
@@ -7,6 +7,8 @@ interface LiveMatchesProps {
 }
 
 const LiveMatches = ({ todayMatches }: LiveMatchesProps) => {
+  const getScore = (score: number | null) => (score === null ? 0 : score);
+
   return (
     <div className="flex flex-col h-screen w-full bg-neutral-900">
       <Card className="m-2 bg-zinc-700">
@@ -29,60 +31,41 @@ const LiveMatches = ({ todayMatches }: LiveMatchesProps) => {
             <Divider className="bg-yellow-400" />
           </div>
           <div className="flex flex-col space-y-1 items-center justify-center">
-            {todayMatches.map((match) => (
-              <div
-                key={match.id}
-                className="flex flex-col p-3 m-2 gap-[10px] bg-neutral-800 rounded-xl w-full max-w-md"
-              >
-                {/* <div className="flex items-center justify-center mb-2">
-                  <div className="flex items-center justify-center w-20 h-20">
-                    <div className="w-16 h-16 flex items-center justify-center">
-                      <Image
-                        src={match.homeTeam.crest}
-                        alt={"Logo do Time 1"}
-                        width={65}
-                        height={65}
-                        className="object-contain"
-                      />
+            {todayMatches //verifica se as partidas de hoje são do brasileirão
+              .filter((match) => match.competition.name === "Campeonato Brasileiro Série A")
+              .map((match) => (
+                <div
+                  key={match.id}
+                  className="flex flex-col p-3 m-2 gap-[10px] bg-neutral-800 rounded-xl w-full max-w-md"
+                >
+                  <div className="px-1">
+                    <Divider className="bg-zinc-700" />
+                  </div>
+                  <div className="flex flex-col items-center">
+                    <div className="flex items-center text-center justify-center text-white text-lg mb-2 gap-2">
+                      <span className="font-semibold text-sm">
+                        {match.homeTeam.name}
+                      </span>{" "}
+                      <span className="flex font-semibold items-center justify-center w-8 h-8 bg-yellow-400">
+                        {getScore(match.score.fullTime.home)}
+                      </span> x{" "}
+                      <span className="flex font-semibold items-center justify-center w-8 h-8 bg-yellow-400">
+                        {getScore(match.score.fullTime.away)}
+                      </span>{" "}
+                      <span className="font-semibold text-sm">
+                        {match.awayTeam.name}
+                      </span>
+                    </div>
+                    <div className="text-center text-gray-400 text-sm">
+                      {new Date(match.utcDate).toLocaleTimeString([], {
+                        hour: "2-digit",
+                        minute: "2-digit",
+                      })}{" "}
+                      | {match.competition.name}
                     </div>
                   </div>
-                  <span className="text-white mx-4">vs</span>
-                  <div className="flex items-center justify-center w-20 h-20">
-                    <div className="w-16 h-16 flex items-center justify-center">
-                      <Image
-                        src={match.awayTeam.crest}
-                        alt={"Logo do Time 2"}
-                        width={65}
-                        height={65}
-                        className="object-contain"
-                      />
-                    </div>
-                  </div>
-                </div> */}
-                <div className="px-1">
-                  <Divider className="bg-zinc-700" />
                 </div>
-                <div className="flex flex-col items-center">
-                  <div className="flex items-center text-center justify-center text-white text-lg mb-2 gap-2">
-                    <span className="font-semibold text-sm">
-                      {match.homeTeam.name}
-                    </span>{" "}
-                    <span className="flex font-semibold items-center justify-center w-8 h-8 bg-yellow-800">{match.score.fullTime.home}</span> x{" "}
-                    <span className="flex font-semibold items-center justify-center w-8 h-8 bg-yellow-800">{match.score.fullTime.away}</span>{" "}
-                    <span className="font-semibold text-sm">
-                      {match.awayTeam.name}
-                    </span>
-                  </div>
-                  <div className="text-center text-gray-400 text-sm">
-                    {new Date(match.utcDate).toLocaleTimeString([], {
-                      hour: "2-digit",
-                      minute: "2-digit",
-                    })}{" "}
-                    | {match.competition.name}
-                  </div>
-                </div>
-              </div>
-            ))}
+              ))}
           </div>
         </CardBody>
       </Card>
